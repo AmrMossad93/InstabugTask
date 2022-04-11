2f041966b4a94b2f4825fe0eead3aeabfba0b367
AmrMossad93 <eng.amrmossad93@hotmail.com>
2022-04-11 16:26:09 +0200

    NF: Chart

--- src/app/Pages/Team-Performance/team-performance/team-performance.component.ts
+++ src/app/Pages/Team-Performance/team-performance/team-performance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-team-performance',
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamPerformanceComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  data() {
+    return {
+      chartData: [
+        {
+          date_ms: 1641772800000,
+          performance: 0.2,
+        },
+        {
+          date_ms: 1641859200000,
+          performance: 0.33,
+        },
+        {
+          date_ms: 1641945600000,
+          performance: 0.53,
+        },
+        {
+          date_ms: 1642032000000,
+          performance: 0.31,
+        },
+        {
+          date_ms: 1642118400000,
+          performance: 0.65,
+        },
+        {
+          date_ms: 1642204800000,
+          performance: 0.88,
+        },
+        {
+          date_ms: 1642291200000,
+          performance: 0.07,
+        },
+      ],
+    };
+  }
 }
